f10bed20d0becc5bf8ba8758f4f167a1bec3a8c7
Jorge Muñoz <jorgemartin.dev@gmail.com>
2025-09-18 12:31:57 -0500

    up 8

--- src/fns.ts
+++ src/fns.ts
@@ -6,8 +6,8 @@ export function randomInt(rng: Randomizer) {
   }
 }
 
-export function shuffle<T>(rng: Randomizer) {
-  return (array: T[]): T[] => {
+export function shuffle(rng: Randomizer) {
+  return <T>(array: T[]): T[] => {
     const shuffled = array.slice()
     for (let i = shuffled.length - 1; i > 0; i--) {
       const j = Math.floor(rng() * (i + 1))
--- tests/core.test.ts
+++ tests/core.test.ts
@@ -1,10 +1,14 @@
 import { it, expect } from 'vitest'
 import wildit, { randomInt, shuffle } from '../src/index'
 
-const randomizer = wildit(18919841618, { randomInt })
+const randomizer = wildit(18915616, { randomInt, shuffle })
 
 it('basic', () => {
   const intValue = randomizer.randomInt(-100, 100)
+
+  const shuffle = randomizer.shuffle(['a', 'b', 'c', 'd', 'e'])
+
+  console.log(shuffle)
 
   expect(randomizer).toBeDefined()
   expect(typeof intValue).toBe('number')
